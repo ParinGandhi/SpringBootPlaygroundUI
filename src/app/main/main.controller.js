bfa2a7982337c21106de07d5cc34436e45a8b4e8
Parin Gandhi <parin.a.gandhi@gmail.com>
2017-08-19 11:15:44 -0400

    Added functionality to create a person

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,11 +6,26 @@
         .controller('MainController', function ($http, $log) {
 
             var vm = this;
+            vm.person = {};
 
             vm.getContactList = function () {
                 $http.get("http://localhost:8080/person/all")
                     .then(function (successResponse) {
                         vm.contactInfo = successResponse.data;
+                    }, function (failureResponse) {
+                        $log.debug(failureResponse);
+                    });
+            };
+
+            vm.addPerson = function () {
+                $http({
+                        url: "http://localhost:8080/person/addPerson",
+                        method: "POST",
+                        data: vm.person
+                    })
+                    .then(function (successResponse) {
+                        $log.debug(successResponse);
+                        vm.getContactList();
                     }, function (failureResponse) {
                         $log.debug(failureResponse);
                     });
